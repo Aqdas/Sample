c831e2ae093fe9f6d7cead03ccd2925b55119eb4
Aqdas <aqdas.rehman@arbisoft.com>
2021-10-04 02:44:00 +0500

    new practice python programs

---
+++ python-course/distance_calculate.py
@@ -0,0 +1,3 @@
+kilometres = float (input ("Please enter the number of kilometres you commute daily: "))
+miles = kilometres / 1.609344
+print (kilometres, "kilometres = ", round(miles, 4), "miles")
